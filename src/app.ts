c4ba339a57f2d2fca215aace724bfadeddd099c2
Saad Srabon <saadsrabon2@gmail.com>
2023-05-31 13:56:20 +0600

    Eslint and Lint staged added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,8 @@
 import express, { Application } from 'express'
-const app:Application = express()
-
+const app: Application = express()
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-
-export default app;
+export default app
