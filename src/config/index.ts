55a30b5fbeed1ff2aaa0f89a17a22028e7b26b62
Saad Srabon <saadsrabon2@gmail.com>
2023-05-31 13:54:28 +0600

    check

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({path:path.join(process.cwd(), '.env')});
+import dotenv from 'dotenv'
+import path from 'path'
+dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
-    port: process.env.PORT,
-    dataBaseUrl: process.env.DATABASE_URL,
+  port: process.env.PORT,
+  dataBaseUrl: process.env.DATABASE_URL,
 }
